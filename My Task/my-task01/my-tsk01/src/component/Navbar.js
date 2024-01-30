bbdbf9e192f218b53c46058d486ec8e72b25ed49
Yasvisenjaliya <yasvisenjaliya2@gmail.com>
2024-01-30 13:31:16 +0530

    commit

--- My Task/my-task01/my-tsk01/src/component/Navbar.js
+++ My Task/my-task01/my-tsk01/src/component/Navbar.js
@@ -1,11 +1,18 @@
-import React from "react";
-import { IoMdSearch } from "react-icons/io";
+import React, { useState } from "react";
+import { IoMdMenu, IoMdSearch } from "react-icons/io";
 import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import "./Nav.css";
 
 const Navbar = ({ size, setShow }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+    <div className="shadow-md xl:bg-white lg:bg-white md:bg-white sm:bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
       <div>
         <div className=" bg-slate-400 py-2">
           <div className="container flex justify-between items-center">
@@ -21,7 +28,7 @@ const Navbar = ({ size, setShow }) => {
                 </h1>
               </div>
             </Link>
-            <div className="flex justify-between items-center gap-4">
+            <div className="flex justify-between items-center lg:mr-10 gap-4">
               <div className="relative group hidden sm:block">
                 <input
                   type="text"
@@ -31,7 +38,10 @@ const Navbar = ({ size, setShow }) => {
                 <IoMdSearch className="text-gray-500 group-hover:text-red-300 absolute top-1/2 -translate-y-1/2 right-3" />
               </div>
               <Link to="/cart">
-                <div onClick={() => setShow(false)} className=" ml-10 text-2xl">
+                <div
+                  onClick={() => setShow(false)}
+                  className=" xl:ml-10 flex lg:mr-5 text-2xl"
+                >
                   <FaShoppingCart />
                   <span>{size}</span>
                 </div>
@@ -42,8 +52,8 @@ const Navbar = ({ size, setShow }) => {
       </div>
       {/* lower navbar */}
 
-      <nav data-aos="zoom-in" className="flex justify-center">
-        <ul className="sm:flex hidden items-center gap-6">
+      {/* <nav data-aos="zoom-in" className="flex justify-center sm:bg-slate-200">
+        <ul className=" flex items-center xl:gap-6 lg:gap-6 md:gap-6 sm:gap-6 max-sm:gap-2 max-sm:grid grid-cols-3 font-semibold">
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -63,6 +73,78 @@ const Navbar = ({ size, setShow }) => {
             <Link to="/exhibition">Exhibition</Link>
           </li>
         </ul>
+      </nav> */}
+
+      <nav
+        data-aos="zoom-in"
+        className="flex justify-center  sm:bg-slate-200  "
+      >
+        <div className=" mx-auto px-8 sm:px-6 lg:px-8">
+          <div className=" h-10">
+            <div className="flex-shrink-0">{/* Your logo or branding */}</div>
+            <div className="hidden sm:block  ">
+              <ul className="flex space-x-6 font-semibold">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/paintings">Paintings</Link>
+                </li>
+                <li>
+                  <Link to="/books">Books</Link>
+                </li>
+                <li>
+                  <Link to="/handcraft">HandCraft</Link>
+                </li>
+                <li>
+                  <Link to="/plant">Plant</Link>
+                </li>
+                <li>
+                  <Link to="/exhibition">Exhibition</Link>
+                </li>
+              </ul>
+            </div>
+            {/* menu for small screens */}
+            
+            <div onClick={toggleMenu}  className="flex sm:hidden text-2xl ml-[23rem]">
+              
+              <IoMdMenu className="block h-6 menu text-slate-300 "/>
+              
+            </div>
+            
+          </div>
+        </div>
+        {/* Responsive dropdown menu */}
+        {isMenuOpen && (
+          <div className="sm:hidden max-sm:w-40 ">
+            <ul
+              className={ 
+                isMenuOpen
+                  ? "nav-menu active"
+                  : "nav-menu pt-2 pb-3 space-y-1 font-semibold"
+              }
+            >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/paintings">Paintings</Link>
+              </li>
+              <li>
+                <Link to="/books">Books</Link>
+              </li>
+              <li>
+                <Link to="/handcraft">HandCraft</Link>
+              </li>
+              <li>
+                <Link to="/plant">Plant</Link>
+              </li>
+              <li>
+                <Link to="/exhibition">Exhibition</Link>
+              </li>
+            </ul>
+          </div>
+        )}
       </nav>
     </div>
   );
--- My Task/my-task01/my-tsk01/src/pages/Paintings.js
+++ My Task/my-task01/my-tsk01/src/pages/Paintings.js
@@ -4,7 +4,7 @@ import { Paint } from "../component/paintingsData";
 const Paintings = ({ handleClick }) => {
   return (
     <div className=" mr-0">
-      <div className=" text-white grid grid-cols-3 xl:gap-1 lg:gap-10 xl:ml-28 lg:ml-5 md:ml-20 max-sm:ml-5 max-sm:mr-5 md:mr-20 max-sm:mt-6 xl:mr-10 lg:mr-5  xl:mt-10 lg:mt-10 md:mt-20">
+      <div className=" text-white xl:grid grid-cols-3 xl:gap-1 lg:gap-10 xl:ml-28 lg:ml-5 md:ml-20 max-sm:ml-5 max-sm:mr-5 md:mr-20 max-sm:mt-6 xl:mr-10 lg:mr-5  xl:mt-10 lg:mt-10 md:mt-20">
         {Paint.map((item) => (
           <div
             key={item.id}
@@ -15,7 +15,7 @@ const Paintings = ({ handleClick }) => {
             </div>
             <div>
               <h1 className="ml-8 mr-2 ">{item.description}</h1>
-              <h1 className=" pl-8 text-teal-600 font-bold">{item.Price} </h1>
+              <h1 className=" pl-8 text-teal-600 font-bold">Rs - {item.Price} </h1>
               <h1 className=" pl-8  text-slate-300">{item.title} </h1>
             </div>
             <button
